0d408070007d91c76a7c845a9859082a23ce8403
Dylan Tng <deelan-@hotmail.com>
2017-10-11 20:52:58 +0800

    First Commit
    First Commit

---
+++ SecurityWebApp/SecurityWebApp/downloadFile.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SecurityWebApp
+{
+    public partial class downloadFile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GenerateDownloadLinks();
+        }
+
+        private void GenerateDownloadLinks()
+        {
+            List<FileClass> lstFiles = new List<FileClass>();
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data/");
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo di = new DirectoryInfo(path);
+
+                foreach(FileInfo fi in di.GetFiles())
+                {
+                    lstFiles.Add(new FileClass() {
+                        FileName = fi.Name,
+                        DownloadLink = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Data/" + fi.Name)
+                    });
+                }
+                DataListContent.DataSource = lstFiles;
+                DataListContent.DataBind();
+            }
+        }
+
+        public class FileClass
+        {
+            public string FileName { get; set; }
+            public string DownloadLink { get; set; }
+        }
+
+        protected void deleteFile(object sender, EventArgs e)
+        {
+            string filePath = (sender as LinkButton).CommandArgument;
+            File.Delete(filePath);
+            GenerateDownloadLinks();
+        }
+
+
+        protected void ButtonDownload_Click(object sender, EventArgs e)
+        {
+            string path = (sender as LinkButton).CommandArgument;
+            string name = Path.GetFileName(path);
+            string ext = Path.GetExtension(path);
+
+            HttpResponse response = HttpContext.Current.Response;
+            response.ClearContent();
+            response.Clear();
+            response.AddHeader("content-disposition", "attachment; filename =" + name);
+            response.ContentType = "text/plain";
+            response.TransmitFile(path);
+            response.Flush();
+            response.End();
+
+        }
+    }
+}
---
+++ SecurityWebApp/SecurityWebApp/uploadFile.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace SecurityWebApp
+{
+    public partial class uploadFile : System.Web.UI.Page
+    {
+        protected System.Web.UI.HtmlControls.HtmlInputFile File1;
+        protected System.Web.UI.HtmlControls.HtmlInputFile File2;
+        protected System.Web.UI.HtmlControls.HtmlInputButton Submit1;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if ((File1.PostedFile != null) && (File1.PostedFile.ContentLength > 0))
+            {
+                string fn = System.IO.Path.GetFileName(File1.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("Data") + "\\" + fn;
+                try
+                {
+                    File1.PostedFile.SaveAs(SaveLocation);
+                    Response.Write("The file has been uploaded.");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Error: " + ex.Message);
+                    //Note: Exception.Message returns a detailed message that describes the current exception. 
+                    //For security reasons, we do not recommend that you return Exception.Message to end users in 
+                    //production environments. It would be better to put a generic error message. 
+                }
+            }
+            else
+            {
+                Response.Write("Please select a file to upload.");
+            }
+        }
+    }
+}
